0ad9326c3e0f73c6427cf75daa49ca819087697a
Mathis Bardot <mathisbardot69@gmail.com>
2023-11-09 09:31:34 +0100

    premier carreaux a modifier

--- snake_écran.py
+++ snake_écran.py
@@ -8,8 +8,33 @@ Width=400
 ClockFrequency=1
 White=(255, 255, 255)
 Black=(0,0,0)
+l=10
 
 screen = pygame.display.set_mode( ( Width,Height) )
+
+screen.fill(White)
+
+#i=0
+#while i <= Height:
+#    
+#    for m in range(l):
+#        j= l
+#        while j <= Width:
+#            for k in range(l):
+#                screen.set_at((j+k, i+m), Black) 
+#            j=j+2*l
+#    i=i+l
+rect = pygame.Rect(0, 0, l, l)
+pygame.draw.rect(screen, Black, rect)
+i=0
+while i <= Height:
+    j=0
+    while j<= Width:
+        rect = pygame.Rect(j, i, l, l)
+        pygame.draw.rect(screen, Black, rect)
+        j+=l
+    i+=l
+
 
 clock = pygame.time.Clock()
 
@@ -24,6 +49,5 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
 
-    screen.fill(White)
 
     pygame.display.update()
