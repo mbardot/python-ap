3ed24db9a9810a9266f5418ce9b24cae4ca1b747
Mathis Bardot <mathisbardot69@gmail.com>
2023-11-09 09:37:32 +0100

    tableau carreaux fonctionnel
    j'ai aussi modifié le quit() pour afficher 0

--- snake_écran.py
+++ snake_écran.py
@@ -8,22 +8,12 @@ Width=400
 ClockFrequency=1
 White=(255, 255, 255)
 Black=(0,0,0)
-l=10
+l=20
 
 screen = pygame.display.set_mode( ( Width,Height) )
 
 screen.fill(White)
 
-#i=0
-#while i <= Height:
-#    
-#    for m in range(l):
-#        j= l
-#        while j <= Width:
-#            for k in range(l):
-#                screen.set_at((j+k, i+m), Black) 
-#            j=j+2*l
-#    i=i+l
 rect = pygame.Rect(0, 0, l, l)
 pygame.draw.rect(screen, Black, rect)
 i=0
@@ -32,7 +22,13 @@ while i <= Height:
     while j<= Width:
         rect = pygame.Rect(j, i, l, l)
         pygame.draw.rect(screen, Black, rect)
-        j+=l
+        j+=2*l
+    i+=l
+    j=l
+    while j<= Width:
+        rect = pygame.Rect(j, i, l, l)
+        pygame.draw.rect(screen, Black, rect)
+        j+=2*l
     i+=l
 
 
@@ -45,8 +41,10 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_q:
+                quit(0)
                 pygame.quit()
         if event.type == pygame.QUIT:
+            pygame(0)
             pygame.quit()
 
 
