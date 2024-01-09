8997764ae35b5c12cd333bc354a0a4fb9eaeb386
Mathis Bardot <mathisbardot69@gmail.com>
2024-01-09 13:57:09 +0100

    end of holidays

--- game_of_life.py
+++ game_of_life.py
@@ -8,18 +8,73 @@ import os
 
 class Cell:
 
-    def __init__(self, position) -> None:
+    def __init__(self, position, state) -> None:
         self._position = position # tupple (line, column)
+        self._state = state # string 'dead' or 'alive'
+    
+    def is_alive(self):
+        if self._state == 'alive':
+            return True
+        return False
     
     def get_position(self):
+        
         return self._position
 
+    def get_neighbors(self, Set_Of_cells):
+        neighbors = []
+        x = self.get_position()[0]
+        y = self.get_position()[1]
+        if Cell( (x-1, y-1 ), 'dead' ) not in Set_Of_cells:#top left
+            neighbors.append( Cell( (x-1, y-1 ), 'dead' ))
+        else:
+            neighbors.append( Cell( (x-1, y-1 ), 'alive' ))
+        
+        if Cell( (x-1, self.get_position()[1] ), 'dead') not in Set_Of_cells:#top
+            neighbors.append( Cell( (x-1, y ), 'dead') )
+        else:
+            neighbors.append( Cell( (x-1, y ), 'alive') )
+        
+        if Cell( (x-1, y+1 ), 'dead')  not in Set_Of_cells:#top right
+            neighbors.append( Cell( (x-1, y+1 ), 'dead') )
+        else:
+            neighbors.append( Cell( (x-1, y+1 ), 'alive') )
+    
+        if Cell( (x, y-1 ), 'dead')  not in Set_Of_cells:#left
+            neighbors.append( Cell( (x, y-1 ), 'dead') )
+        else:
+            neighbors.append( Cell( (x, y-1 ), 'alive') )
+
+        if Cell( (x, y+1 ), 'dead') not in Set_Of_cells:#right
+            neighbors.append( Cell( (x, y+1 ), 'dead') )
+        else:
+            neighbors.append( Cell( (x, y+1 ), 'alive') )
+
+        if Cell( (x+1, y-1 ), 'dead') not in Set_Of_cells:#bottom left
+            neighbors.append( Cell( (x+1, y-1 ), 'dead') )
+        else:
+            neighbors.append( Cell( (x+1, y-1 ), 'alive') )
+
+        if Cell( (x+1, y ), 'dead') not in Set_Of_cells:#bottom
+            neighbors.append( Cell( (x+1, y ), 'dead') )
+        else:
+            neighbors.append( Cell( (x+1, y ), 'alive') )
+
+        if Cell( (x+1, y+1 ), 'dead') not in Set_Of_cells:#bottom right
+            neighbors.append( Cell( (x+1, y+1 ), 'dead') )
+        else:
+            neighbors.append( Cell( (x+1, y+1 ), 'alive') )
+        
+        return neighbors
 
 class Set_Of_Cells:
 
     def __init__(self, list_of_cells) -> None:
         self._set = list_of_cells
 
+    def __iter__(self):
+        return iter(self._set)
+    
     def display(self, screen, l):
         for cell in self._set:
             pos = cell.get_position()
@@ -31,14 +86,64 @@ class Set_Of_Cells:
             with open(path) as file:
                 for l, line in enumerate(file):
                     for c, case in enumerate(line):
-                        if case == '1':#dead cell
-                            self._set.append( (l, c) )
+                        if case == '1':#living cell
+                            self._set.append( Cell((l, c), 'alive') )
         else:
             logging.error('input file not found')
 
     def save_state(self): #TODO
         pass
 
+    def to_visit(self): #returns a list of all interesting cells to check for this turn
+        return get_neighbors(self._set)
+
+    def check_all(self):   #we go through all of the 'interesting cells'
+        new_set = []
+        cells_to_visit = self.to_visit()
+        print(cells_to_visit)
+        for cell in cells_to_visit:
+            neighbors = cell.get_neighbors(self) #these are the direct neighbors of the cell 'cell', there state decide if cell lives or not
+            alive = alive_in(neighbors) #int
+            if cell.is_alive():
+                if alive == 2 or alive == 3:
+                    new_set.append( cell)
+            else:
+                if alive == 3:
+                    new_set.append( cell)
+        return Set_Of_Cells(new_set)
+
+
+
+def get_neighbors(cells): # list of cell  -> list of cell
+    neighbors = cells
+    for cell in cells:
+        x = cell.get_position()[0]
+        y = cell.get_position()[1]
+        if Cell( (x-1, y-1 ), 'alive' ) not in neighbors:#top left
+            neighbors.append( Cell( (x-1, y-1 ), 'dead' ))  
+        if Cell( (x-1, y ), 'alive') not in neighbors:#top
+            neighbors.append( Cell( (x-1, y ), 'dead') )
+        if Cell( (x-1, y+1 ), 'alive')  not in neighbors:#top right
+            neighbors.append( Cell( (x-1, y+1 ), 'dead') )
+        if Cell( (x, y-1 ), 'alive')  not in neighbors:#left
+            neighbors.append( Cell( (x, y-1 ), 'dead') )
+        if Cell( (x, y+1 ), 'alive') not in neighbors:#right
+            neighbors.append( Cell( (x, y+1 ), 'dead') )
+        if Cell( (x+1, y-1 ), 'alive') not in neighbors:#bottom left
+            neighbors.append( Cell( (x+1, y-1 ), 'dead') )
+        if Cell( (x+1, y ), 'alive') not in neighbors:#bottom
+            neighbors.append( Cell( (x+1, y ), 'dead') )
+        if Cell( (x+1, y+1 ), 'alive') not in neighbors:#bottom right
+            neighbors.append( Cell( (x+1, y+1 ), 'dead') )
+    
+    return neighbors
+
+def alive_in(list):
+    n = 0
+    for cell in list:
+        if cell.is_alive():
+            n += 1
+    return n
 
 def add_arguments(parser):
     parser.add_argument('-i', help="to set the path to the initial pattern file")
@@ -55,16 +160,24 @@ def add_arguments(parser):
 
 def draw(screen, cells, l):
     #display dead cells
+    screen.fill('white')
     cells.display(screen, l)
     pygame.display.set_caption('Game Of Life')
+    print('+++++++++++++++++++++++++++++++++++')
     pygame.display.update()
 
+
+def process_events():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            return False
+    return True
 
 def main():
 
     #take potential inputs
     parser = argparse.ArgumentParser(description='Some description')
-    parser = add_arg(parser)#removed the big block
+    parser = add_arguments(parser)#removed the big block
     args = parser.parse_args()
 
     #define local (to main) constants
@@ -72,7 +185,8 @@ def main():
     HEIGHT = args.height
     WIDTH = args.width
     L = 10
-    cells = Set_Of_Cells([])
+    clock = pygame.time.Clock()
+    cells = Set_Of_Cells([])#this is the set of living cells
     cells.load_state(args.i)
 
     # Configure logging
@@ -80,7 +194,6 @@ def main():
     logging.basicConfig(level=log_level, format='%(asctime)s - %(levelname)s - %(message)s')
 
     if args.d : #we want to display with pygame
-    
         logging.info('we display')
         pygame.init()
         execute = True
@@ -91,10 +204,14 @@ def main():
             clock.tick(CLOCK_FREQUENCY)
 
             draw(screen, cells, L)
-
-
+            
+            cells = cells.check_all()#updates who lives, who dies
+            print('here')
+            execute = process_events()
 
     else: #we do not want to display:
         logging.info("we don't display")
         for step in range(args.m):
-            
+            pass
+
+main()
